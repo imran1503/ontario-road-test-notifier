6f42ea36e2e9d75ec98627f40117ae3f4b90b13a
Imran Latif <imran.latif1503@gmail.com>
2026-09-17 17:34:21 -0400

    Added retry

--- check_dates.py
+++ check_dates.py
@@ -1,7 +1,17 @@
 import requests
+from urllib3 import Retry
 
 URL = "https://www.roadtestnotification.ca/available-dates/"
 
+retry = Retry(
+    total=4,
+    connect=4,
+    read=4,
+    backoff_factor=2,
+    status_forcelist=[429, 500, 502, 503, 504],
+    allowed_methods=["GET"],
+    respect_retry_after_header=True,
+)
 response = requests.get(URL)
 
 print(response.status_code)
